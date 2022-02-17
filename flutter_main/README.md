964272f35b988164cbfd457d48c20896d1f30388
daijinload <daijinload@gmail.com>
2022-02-17 16:15:18 +0900

    feature: サンプルを残しつつ、mainプロジェクトを作成

---
+++ flutter_main/README.md
@@ -0,0 +1,20 @@
+# flutter_main
+
+
+## setup
+
+```bash
+flutter pub get
+flutter analyze
+flutter test
+
+# 単品実行
+flutter test test/widget/aaa_widget_test.dart
+
+# おかしくなったらクリーンする
+flutter clean
+```
+
+
+
+
---
+++ flutter_main/lib/view/widget/aaa_widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class AaaWidget extends StatelessWidget {
+  const AaaWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter += 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'ボタンを押したら数字がインクリメントされます。',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
