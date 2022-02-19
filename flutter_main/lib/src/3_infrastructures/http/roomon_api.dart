d98af2b848057222981ef28e4e33266c3c108b49
daijinload <daijinload@gmail.com>
2022-02-19 16:40:57 +0900

    [main] refactor: http周りとviewsのrootディレクトリ名称変更 (#5)

--- flutter_main/lib/src/3_infrastructures/http/roomon_api.dart
+++ flutter_main/lib/src/3_infrastructures/http/roomon_api.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
 
@@ -28,10 +29,17 @@ Future<String> getApi(String path) async {
   var client = HttpClient();
   try {
     HttpClientRequest request = await client.get('127.0.0.1', 8090, path);
-    HttpClientResponse response = await request.close();
+    HttpClientResponse response =
+        await request.close().timeout(const Duration(seconds: 3));
     final stringData = await response.transform(utf8.decoder).join();
     // print(stringData.toString());
     return stringData.toString();
+  } on TimeoutException catch (_) {
+    // A timeout occurred.
+    rethrow;
+  } on SocketException catch (_) {
+    // Other exception
+    rethrow;
   } finally {
     client.close();
   }
