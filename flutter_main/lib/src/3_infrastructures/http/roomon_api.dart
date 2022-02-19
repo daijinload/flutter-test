3a04c0f012d2ab848bc7338a3d3a4c1cab7440f0
daijinload <daijinload@gmail.com>
2022-02-19 16:44:32 +0900

    aaa

--- flutter_main/lib/src/3_infrastructures/http/roomon_api.dart
+++ flutter_main/lib/src/3_infrastructures/http/roomon_api.dart
@@ -2,25 +2,6 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
 
-// void main() async {
-//   // final stringData = await getApi("/");
-//   // print(stringData);
-//   final stringData = await getUserInfo();
-//   print(stringData);
-// }
-// void main() async {
-//   var client = HttpClient();
-//   try {
-//     HttpClientRequest request = await client.get('localhost', 8090, '/');
-//     HttpClientResponse response = await request.close();
-//     final stringData = await response.transform(utf8.decoder).join();
-//     // ignore: avoid_print
-//     print(stringData);
-//   } finally {
-//     client.close();
-//   }
-// }
-
 Future<String> getUserInfo() async {
   return await getApi("/");
 }
@@ -44,22 +25,3 @@ Future<String> getApi(String path) async {
     client.close();
   }
 }
-
-// feature<string> pumpWidget() {
-//     return "";
-// }
-//  （1）定義に沿った関数例
-// String helloWorld1(String world){
-//   return 'Hello $world';
-// }
-// //  （2）戻り値と引数の型定義を省略
-// helloWorld2(world){
-//   return 'Hello $world';
-// }
-// //  （3） 1行関数での記述例
-// String helloWorld4(String world) => 'Hello $world';
-
-// //  （4）戻り値がいらない場合
-// void helloWorld3(){
-//   print('Hello');
-// }
