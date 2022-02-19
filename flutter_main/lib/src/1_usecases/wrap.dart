af498a4566bc460af419981d368ed67f921589fd
daijinload <daijinload@gmail.com>
2022-02-19 19:08:28 +0900

    [main] feature: サービスロケータパターンとデコレータパターンの記述比べ追加 (#7)

---
+++ flutter_main/lib/src/1_usecases/wrap.dart
@@ -0,0 +1,6 @@
+Future<dynamic> exec(Function fff, Function callback) {
+  // ignore: avoid_print
+  print("called function wrap関数:${fff.toString()}");
+  final result = callback();
+  return result;
+}
--- flutter_main/lib/views/2_page/user_page.dart
+++ flutter_main/lib/views/2_page/user_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_main/src/1_usecases/container.dart' as container;
+import 'package:flutter_main/src/1_usecases/user_usecase.dart' as user_usecase;
+import 'package:flutter_main/src/1_usecases/wrap.dart' as wrap;
 
 class UserPage extends StatefulWidget {
   const UserPage({Key? key, required this.title}) : super(key: key);
@@ -11,11 +13,27 @@ class UserPage extends StatefulWidget {
 class UserPageState extends State<UserPage> {
   String str = "";
 
-  Future<void> userInfo() async {
+  Future<void> onPressedUserInfo() async {
     // 起動時に書くべきだが、サンプルなので、ここでセットアップする
     container.setUp();
 
-    // 処理を取得して、httpリクエストする
+    // デコレータパターン
+    // サービスロケータパターンよりも型チェックが効くが見た目ごついし、
+    // 実行関数とパラメータを渡さないと、何がどのパラメータで実行されている？
+    // というログは取れないパターンとなる。
+    // コンテナ側にコードを追記していく作業は無くなるし、
+    // IDEでの関数コール検索ができ、使っている箇所の特定が容易になる。
+    // データのモック化まで考慮に入れると、execにモック処理を書く必要はあるかと。
+    var resultbk = await wrap.exec(user_usecase.userInfo, () {
+      return user_usecase.userInfo('');
+    });
+    print(resultbk);
+
+    // サービスロケータパターン
+    // 型チェックが効かなくなるが、ログなど見やすく出せると思われるパターン。
+    // コンテナ側にユースケースを作成するたびに追加しなければならず、大きくなる。
+    // リフレクションで取ってこようとしたが、それだと全てのコードが必要になりカリングができず、
+    // バイナリサイズが大きくなる的な話があったため、importを地道に書くことにした。
     var result = await container.get("/user/info")("uuu");
     setState(() {
       str = result;
@@ -43,7 +61,7 @@ class UserPageState extends State<UserPage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: userInfo,
+        onPressed: onPressedUserInfo,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
