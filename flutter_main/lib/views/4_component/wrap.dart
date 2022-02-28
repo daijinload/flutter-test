192ecc1cbb142bbcae37afcd11c8cb25ad480289
daijinload <daijinload@gmail.com>
2022-02-28 17:32:06 +0900

    aaa

--- flutter_main/lib/views/4_component/wrap.dart
+++ flutter_main/lib/views/4_component/wrap.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_main/views/4_component/error_dialog_component.dart';
 
@@ -9,6 +8,9 @@ Future<dynamic> exec(BuildContext ctx, Function fff, Function callback) async {
   try {
     final result = await callback();
     return result;
+  // 各々のエラー処理が下記に記述される想定
+  // タイムアウトなら、何回かリトライしたいかもだが、そういうのは
+  // 各インフラストラクチャの処理でやるほうが良さそう。
   // } on TimeoutException catch (e) {
   //   showSystemErrorDialog(ctx, e.toString());
   //   rethrow;
