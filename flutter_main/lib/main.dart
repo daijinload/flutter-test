0aa4416ba1ae720dbc7dbc41bd28fee83c14c647
daijinload <daijinload@gmail.com>
2022-02-28 16:42:04 +0900

    aaa

--- flutter_main/lib/main.dart
+++ flutter_main/lib/main.dart
@@ -2,5 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_main/views/1_root/main_root.dart';
 
 void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // if (kReleaseMode) {
+  //   ErrorWidget.builder = (FlutterErrorDetails details) => SomethingWrong();
+  // }
   runApp(const MainRoot());
 }
