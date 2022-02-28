74d650d3bd62b40ce57f8a02b1e8dbffb191a997
daijinload <daijinload@gmail.com>
2022-02-28 17:39:59 +0900

    aaa

--- flutter_main/lib/views/3_template/wrap_template.dart
+++ flutter_main/lib/views/3_template/wrap_template.dart
@@ -2,10 +2,11 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_main/views/4_component/error_dialog_component.dart';
 
-Future<dynamic> exec(BuildContext ctx, Function fff, Function callback) async {
+Future<dynamic> exec(BuildContext ctx, Function usecaseFunc, Function callback) async {
   // ignore: avoid_print
-  print("★★★★★: called function wrap関数: ${fff.toString()}");
+  print("★★★★★: called function wrap関数: ${usecaseFunc.toString()}");
   try {
+    // ignore: todo 紙芝居モードのときは、引数のユースケース関数をkeyにしたマップが値を返す仕組みにする予定。
     final result = await callback();
     return result;
   // 各々のエラー処理が下記に記述される想定
