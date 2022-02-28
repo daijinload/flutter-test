5ecd2884def06adcec3a836b206a3c5e678bb66f
daijinload <daijinload@gmail.com>
2022-02-28 17:27:40 +0900

    aaa

--- flutter_main/lib/main.dart
+++ flutter_main/lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_main/views/1_root/main_root.dart';
 
 void main() async {
+  // ignore: todo レンダリング側のエラーを補足するかどうか？検討する。
+  // エラー時のデフォルトの動作は、デバッグモードで例外のメッセージを表示することである。
+  // リリースビルドではグレーの背景以外何も表示されない。
+  // なので、本番時にもレンダリング側のエラーを表示したい場合、下記のように設定する。
   // WidgetsFlutterBinding.ensureInitialized();
   // if (kReleaseMode) {
   //   ErrorWidget.builder = (FlutterErrorDetails details) => SomethingWrong();
