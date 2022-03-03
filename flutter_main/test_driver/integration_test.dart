fc7ca5032b00cd91ea5100c9ca6bc493984c20e0
daijinload <daijinload@gmail.com>
2022-03-03 17:31:22 +0900

    aaa

--- flutter_main/test_driver/integration_test.dart
+++ flutter_main/test_driver/integration_test.dart
@@ -1,17 +1,18 @@
 import 'dart:io';
+import 'dart:developer' as developer;
 import 'package:integration_test/integration_test_driver_extended.dart';
 
 Future<void> main() async {
-  await integrationDriver(
-    onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
-      final File image = await File('screenshots/$screenshotName.png')
-          .create(recursive: true);
-      image.writeAsBytesSync(screenshotBytes);
-      return true;
-    },
-  );
-  // try {
-  // } catch (e) {
-  //   print('Error occured: $e');
-  // }
+  try {
+    await integrationDriver(
+      onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
+        final File image = await File('screenshots/$screenshotName.png')
+            .create(recursive: true);
+        image.writeAsBytesSync(screenshotBytes);
+        return true;
+      },
+    );
+  } catch (e) {
+    developer.log('Error occured: $e');
+  }
 }
