0113aa95669e6b7c3d6def76822de99274051de9
daijinload <daijinload@gmail.com>
2022-03-18 21:18:50 +0900

    aaa

---
+++ flutter_main/lib/src/3_infrastructures/graphql/test_gql.dart
@@ -0,0 +1,16 @@
+
+import 'package:artemis/artemis.dart';
+import 'package:flutter_main/src/3_infrastructures/graphql/generated/test.dart';
+
+final client = ArtemisClient('http://localhost:8040/gql/device');
+
+Future<String> echo() async {
+  final echoQuery = EchoQuery();
+  final response = await client.execute(echoQuery);
+  if (response.hasErrors) {
+    // TODO どこかで
+    throw Exception(response.errors!.first.message);
+  }
+  
+  return response.data!.echo!.id;
+}
